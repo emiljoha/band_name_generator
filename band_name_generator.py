b52ef9ad8d6bd87a84f3f03c3c044d5accd94693
Emil Johansson <emil@localhost.localdomain>
2018-04-21 00:03:51 +0200

    Implements draw text on image and display.
    It is not pretty, I does not allways fit on the image but text scaped
    of wikipedia and end of random quotes is written on images forming
    cover art ish.

--- band_name_generator.py
+++ band_name_generator.py
@@ -14,12 +14,16 @@
 import wikipedia as wiki
 import wikiquotes
 import flickrapi
+import urllib.request as download
+from PIL import Image, ImageDraw, ImageFont
+import os
 # You need personal api_key and secret from flickr to use api You need
 # to get your own and write a config.py file to get the flickr
 # image.
 from config import api_key, secret
 
-print('Band name: %s' % wiki.random(1).title())
+band_name = wiki.random(1).title()
+print('Band name: %s' % band_name)
 quote = wikiquotes.random_quote(author='Aristotle', raw_language="english")
 album_name = quote.split(' ')[-4:]
 album_name = ' '.join(album_name)
@@ -33,7 +37,7 @@ if api_key == '<api key here>' or secret == '<secret here>':
     print('To get keys to insert into config.py')
     get_album_art = False
 else:
-    get_album_art = False
+    get_album_art = True
 
 if get_album_art is True:
     flickr = flickrapi.FlickrAPI(api_key, secret)
@@ -48,4 +52,19 @@ if get_album_art is True:
                                                             server,
                                                             photo_id,
                                                             photo_secret)
-    print(url)
+    filename = '_'.join(band_name.split(' '))
+    if filename[-1] == '.':
+        filename = filename.split('')
+        del filename[-1]
+        filename = ''.join(filename)
+    filename = 'albums/%s.JPEG' % filename
+    download.urlretrieve(url, filename=filename)
+    img = Image.open(filename)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Verdana.ttf", 30)
+    draw.text((0*img.size[1]/4, img.size[1]/4), album_name, font=font,
+              fill=(200, 0, 0, 255))
+    draw.text((0*img.size[1]/4, 3*img.size[1]/4), 'By: %s' % band_name,
+              font=font, fill=(200, 0, 0, 255))
+    img.save(filename, "JPEG", quality=300)
+    os.system('xdg-open %s' % filename)
--- requirements.py
+++ requirements.py
@@ -1,2 +1,3 @@
 wikipedia
 wikiquotes
+pillow
